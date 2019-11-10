1d03db917770d60e6d7cdb1ac4e88a0e431fb3b3
Jan PILLAR <japisoft@hotmail.com>
2019-11-10 15:31:59 +0100

    bonus

---
+++ js/bonus.js
@@ -0,0 +1,10 @@
+﻿const trigger = document.querySelector('.open-modal-btn');
+const closeBtn = document.querySelector('.close-modal-btn');
+const modal = document.querySelector('.my-modal');
+
+trigger.addEventListener('click', () => {
+    modal.showModal();
+});
+closeBtn.addEventListener('click', () => {
+    modal.close();
+});
---
+++ js/progress.js
@@ -0,0 +1,8 @@
+﻿const elem = document.getElementById('dynamic');
+
+setInterval(() => {
+    elem.value += 1;
+    if (elem.value === 100) {
+        elem.value = 0;
+    }
+}, 20);
